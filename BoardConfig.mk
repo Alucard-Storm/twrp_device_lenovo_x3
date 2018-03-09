b066e5881d6ab4360b8a372beb902352a667e838
Alucard Stormbringer <supersayan1000@gmail.com>
2018-03-09 20:59:52 +0530

    removed unneeded flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,6 @@ TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_DEFAULT_BRIGHTNESS := 80
 TW_DEFAULT_EXTERNAL_STORAGE := true
@@ -64,7 +63,6 @@ TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 TW_MTP_DEVICE := x3
-
 
 # Asian region languages
 TW_EXTRA_LANGUAGES := true
